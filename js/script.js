861d59ec67955f90cdbdc09bd27440b652dc19eb
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-11 18:59:29 -0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 /**
 The "Name" field:
-  When page loads, curser should focus on the "name" field
+  When page loads, curser should focus on the "name" field.
 **/
 const name = document.getElementById("name").focus();
+
+/**
+The "Job" section:
+  The form should hide the "Other job role" field when a role from the dropdown menu is chosen.
+  If "Other job role" is chosen, the field to write in another role should display.
+**/
