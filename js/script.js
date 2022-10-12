9e7a56fe1dd337614ba5578b0aaaa190dee62600
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-11 19:19:26 -0700

    Update

--- js/script.js
+++ js/script.js
@@ -9,3 +9,5 @@ The "Job" section:
   The form should hide the "Other job role" field when a role from the dropdown menu is chosen.
   If "Other job role" is chosen, the field to write in another role should display.
 **/
+const jobRole = document.getElementById("title");
+const otherJobField = document.getElementById("other-job-role");
